62c42f5d94ca1afd49033770b182592bbbcf8fd8
espindolasol95 <espindolasol.ok@gmail.com>
2025-07-14 15:18:14 +0200

    feat(creazione di array biciclette)

---
+++ script.js
@@ -0,0 +1,9 @@
+//array di oggeti biciclette
+const biciclette= [
+    {nome: Treck , peso: 10.5 },
+    {nome: Bianchi, peso: 9.5 },
+    {nome: cannoldale, peso: 8.5 },
+    {nome: Pinarello, peso: 7.5 },
+    {nome: canyon, peso: 6.00 },
+
+]
